83972913ad710d66e92a20622883ae41d073bd43
Rafael Cunha Nascimento <DATA54319@datainfo.inf.br>
2023-07-17 11:21:18 -0300

    Criação e Validação de formulário para login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
 
+  email = new FormControl(null, Validators.email); //Validando se é um email com o @
+  senha = new FormControl(null, Validators.minLength(3)); //Validando se a senha tenha pelo menos 3 caract.
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  validaCampos(): boolean {
+    if(this.email.valid && this.senha.valid){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
