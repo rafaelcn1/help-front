d5f9783d915ec2bd6dd9f24466e67dc542e94fc7
Rafael Cunha <rafaelcn1@hotmail.com>
2023-07-24 14:57:41 -0300

    Implementação da Rota para componente tecnico-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './auth/auth.guard';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 
 
@@ -15,7 +16,8 @@ const routes: Routes = [
   {
     path: '',component: NavComponent,  canActivate: [authGuard], children: [
       {path: 'home',component: HomeComponent},
-      {path: 'tecnicos',component: TecnicoListComponent}
+      {path: 'tecnicos',component: TecnicoListComponent},
+      {path: 'tecnicos/create',component: TecnicoCreateComponent} // quando for acessado a url tecnicos/create será reinderizado o TecnicoCreateComponent
     ]
   }
 ];
