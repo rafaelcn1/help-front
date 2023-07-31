5f22a7d163804a353d275781ab9acdaa52345fb1
Rafael Cunha <rafaelcn1@hotmail.com>
2023-07-31 13:32:23 -0300

    Criação da implementação do service do cliente

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Cliente } from '../models/cliente';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private url: string = API_CONFIG.baseUrl + 'clientes/'
+
+  constructor(private http: HttpClient) { }
+
+  findById(id: any): Observable<Cliente> {
+    this.url = this.url + id;
+    return this.http.get<Cliente>(this.url);
+  }
+
+  findAll(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.url);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.url, cliente);
+  }
+
+  update(cliente: Cliente): Observable<Cliente> {
+    this.url = this.url + cliente.id;
+    return this.http.put<Cliente>(this.url, cliente);
+  }
+
+  delete(id: any): Observable<Cliente> {
+    this.url = this.url + id;
+    return this.http.delete<Cliente>(this.url);
+  }
 }
