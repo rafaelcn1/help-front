d90bed5b59ca81270bf97b64a9e4cec07426fce0
Rafael Cunha <rafaelcn1@hotmail.com>
2023-08-01 15:20:26 -0300

    Criação do componente cliente-update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecn
 import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
+import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 
 @NgModule({
   declarations: [
@@ -57,7 +58,8 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
     TecnicoUpdateComponent,
     TecnicoDeleteComponent,
     ClienteListComponent,
-    ClienteCreateComponent
+    ClienteCreateComponent,
+    ClienteUpdateComponent
   ],
   imports: [
     BrowserModule,
