b5a1288080f5f4e8cd188edf7cc293c41665a1a0
Rafael Cunha <rafaelcn1@hotmail.com>
2023-08-07 15:51:42 -0300

    Implementação dos metodos findById e delete no componente cliente-delete

--- src/app/components/cliente/cliente-delete/cliente-delete.component.ts
+++ src/app/components/cliente/cliente-delete/cliente-delete.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { Cliente } from "src/app/models/cliente";
+import { ClienteService } from "src/app/services/cliente.service";
 
 @Component({
-  selector: 'app-cliente-delete',
-  templateUrl: './cliente-delete.component.html',
-  styleUrls: ['./cliente-delete.component.css']
+  selector: "app-cliente-delete",
+  templateUrl: "./cliente-delete.component.html",
+  styleUrls: ["./cliente-delete.component.css"],
 })
-export class ClienteDeleteComponent {
+export class ClienteDeleteComponent implements OnInit {
+  ngOnInit(): void {
+    this.cliente.id = this.activatedRoute.snapshot.paramMap.get("id");
+    this.findById();
+  }
 
+  cliente: Cliente = {
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    perfis: [],
+    dataCriacao: undefined,
+  };
+
+  constructor(
+    private service: ClienteService,
+    private toastr: ToastrService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  findById(): void {
+    this.service.findById(this.cliente.id).subscribe((resposta) => {
+      resposta.perfis = [];
+      this.cliente = resposta;
+    });
+  }
+
+  delete(): void {
+    this.service.delete(this.cliente.id).subscribe((resposta) => {
+      this.toastr.success("Cliente excluído com sucesso!");
+      this.router.navigate(["/clientes"]);
+    }, ex => {
+      if(ex.error.errors){
+        ex.error.errors.forEach(erro => {
+          this.toastr.error(erro.message);
+        })
+      } else {
+        this.toastr.error(ex.error.message);
+      }
+    });
+  }
 }
