7be1ae0fce53ea30aeac8ea83e7945967a2c4272
Rafael Cunha <rafaelcn1@hotmail.com>
2023-08-07 17:03:20 -0300

    Criação da interface do chamado

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,13 @@
+export interface Chamado {
+    id?: any;
+    dataAbertura?: String;
+    dataFechamento?: String;
+    prioridade: String;
+	status: String;
+	titulo: String;
+	observacoes: String;
+    tecnico: any;
+    cliente: any;
+    nomeTecnico: String;
+    nomeCliente: String;
+}
